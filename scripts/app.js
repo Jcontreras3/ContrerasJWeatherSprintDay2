2dfbbe91edf54e6271ecb751c1c89bbaa5ef8102
Jovann Contreras <jcontreras@codestack.co>
2023-01-24 14:58:38 -0800

    added text with creating elements

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,19 @@
 import { WeatherURL, ForeCastURL } from "./api.js";
 
 let weatherCol1 = document.getElementById('weatherCol1');
+let weatherCol2 = document.getElementById('weatherCol2');
 
 function WeatherColElements(){
     let cityText = document.createElement('p');
     cityText.textContent = "Modesto, CA";
     cityText.className = "pTxtStyles1"
+    let currentWTxt = document.createElement('p')
+    currentWTxt.textContent = "CurrentWeather";
+    currentWTxt.className = "pTxtStyles1";
 
 
     weatherCol1.appendChild(cityText);
+    weatherCol2.appendChild(currentWTxt);
 }
 
 WeatherURL();
